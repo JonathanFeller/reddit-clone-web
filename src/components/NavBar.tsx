0ce581760a77efe3fd34fa7ff7b217ecf416ba99
JonathanRicardo <jonricfel@gmail.com>
2020-11-05 21:45:16 -0300

    Added me query & navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,43 @@
 import React from "react";
-
-import { Flex, Link } from "@chakra-ui/core";
 import NextLink from "next/link";
+
+import { Box, Flex, Link, Button } from "@chakra-ui/core";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data }] = useMeQuery();
+  let body = null;
+
+  if (!data?.me) {
+    //Not logged in
+
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link m="0 4px">Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link m="0 4px">Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box m="0 4px">{data.me.username}</Box>
+        <Button m="0 4px" variant="link">
+          Logout
+        </Button>
+      </Flex>
+    );
+    //Logged in
+  }
+
   return (
     <Flex bg="tomato" p={4} justifyContent="flex-end">
-      <NextLink href="/login">
-        <Link p="0 4px">Login</Link>
-      </NextLink>
-      <NextLink href="/register">
-        <Link p="0 4px">Register</Link>
-      </NextLink>
+      {body}
     </Flex>
   );
 };

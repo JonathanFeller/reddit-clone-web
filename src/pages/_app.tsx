c909c5580e2b8d6087a1dc9f81da27c91e00952c
JonathanRicardo <jonricfel@gmail.com>
2020-11-07 21:07:05 -0300

    Added server side rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,89 +1,12 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
-import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
-import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
-import {
-  LoginMutation,
-  LogoutMutation,
-  MeDocument,
-  MeQuery,
-  RegisterMutation,
-} from "../generated/graphql";
 import theme from "../theme";
-
-function updateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: "include",
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          login: (_result, _args, cache, _info) => {
-            updateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.login.user,
-                  };
-                }
-              }
-            );
-          },
-          register: (_result, _args, cache, _info) => {
-            updateQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.register.user,
-                  };
-                }
-              }
-            );
-          },
-          logout: (_result, _args, cache, _info) => {
-            updateQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              () => ({ me: null })
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
       <ThemeProvider theme={theme}>
         <CSSReset />
         <Component {...pageProps} />
       </ThemeProvider>
-    </Provider>
   );
 }
 

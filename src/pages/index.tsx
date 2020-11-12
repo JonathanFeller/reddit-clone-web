f80f4e3a727aaec01b89cf259af86d9fa7ec18ca
JonathanRicardo <jonricfel@gmail.com>
2020-11-11 23:36:14 -0300

    Added pagination to posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,27 @@
 import { withUrqlClient } from "next-urql";
-import { NavBar } from "../components/NavBar";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import { Link } from "@chakra-ui/core";
+import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
   return (
-    <>
-      <NavBar />
-      <div>Hello world</div>
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>Create Post</Link>
+      </NextLink>
       {!data ? (
         <div>Loading...</div>
       ) : (
         data.posts.map((p) => <div key={p.id}>{p.title}</div>)
       )}
-    </>
+    </Layout>
   );
 };
 

374356fac31cb3b6c9a615183acf365cb8147ac5
JonathanRicardo <jonricfel@gmail.com>
2020-11-15 23:58:25 -0300

    Added Load more button to handle pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,21 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
-import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import NextLink from "next/link";
 import React from "react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
+
+  if (!fetching && !data) {
+    return <div>Unexpected error</div>;
+  }
+
   return (
     <Layout>
       <Flex mb={5} alignContent="center" justifyContent="space-between">
@@ -20,17 +25,24 @@ const Index = () => {
           <Link>Create Post</Link>
         </NextLink>
       </Flex>
-      {!data ? (
+      {fetching && !data ? (
         <div>Loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data.posts.map((p) => (
+          {data!.posts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading>{p.title}</Heading>
               <Text>{p.textSnippet}</Text>
             </Box>
           ))}
         </Stack>
+      )}
+      {data && (
+        <Flex justifyContent="center">
+          <Button isLoading={fetching} my={8}>
+            Load more
+          </Button>
+        </Flex>
       )}
     </Layout>
   );

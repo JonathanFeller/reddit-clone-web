e868c7d6587e784527629d02b27c1785f3b23334
JonathanRicardo <jonricfel@gmail.com>
2020-11-19 22:59:38 -0300

    Outsourced Edit & Delete buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,33 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  IconButton,
-  Link,
-  Stack,
-  Text,
-} from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 import React, { useState } from "react";
 import { Layout } from "../components/Layout";
 import { VoteSection } from "../components/VoteSection";
-import {
-  useDeletePostMutation,
-  useMeQuery,
-  usePostsQuery,
-} from "../generated/graphql";
+import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { EditDeletePostButtons } from "../utils/EditDeletePostButtons";
 
 const Index = () => {
   const [variables, setVariables] = useState({
     limit: 15,
     cursor: null as string | null,
   });
-  const [{ data: postData, fetching }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables,
   });
-  const [{ data: meData }] = useMeQuery();
 
-  const [, deletePost] = useDeletePostMutation();
-
-  if (!fetching && !postData) {
+  if (!fetching && !data) {
     return <div>Unexpected error</div>;
   }
 
   return (
     <Layout>
-      {fetching && !postData ? (
+      {fetching && !data ? (
         <div>Loading...</div>
       ) : (
         <Stack spacing={8}>
-          {postData!.posts.posts.map((p) =>
+          {data!.posts.posts.map((p) =>
             !p ? null : (
               <Box key={p.id} p={5} shadow="md" borderWidth="1px">
                 <Flex flexGrow={1}>
@@ -58,40 +43,22 @@ const Index = () => {
                     </Flex>
                     <Text>{p.textSnippet}</Text>
                   </Flex>
-                  {meData?.me?.id !== p.creator.id ? null : (
-                    <Flex flexDirection="column" justifyContent="space-between">
-                      <NextLink
-                        href="/post/edit/[id]"
-                        as={`/post/edit/${p.id}`}
-                      >
-                        <IconButton
-                          as={Link}
-                          icon="edit"
-                          aria-label="Edit Post"
-                        />
-                      </NextLink>
-                      <IconButton
-                        icon="delete"
-                        aria-label="Delete Post"
-                        onClick={() => {
-                          deletePost({ id: p.id });
-                        }}
-                      />
-                    </Flex>
-                  )}
+                  <Flex flexDirection="column" justifyContent="space-between">
+                    <EditDeletePostButtons id={p.id} creatorId={p.creator.id} />
+                  </Flex>
                 </Flex>
               </Box>
             )
           )}
         </Stack>
       )}
-      {postData && postData.posts.hasMore && (
+      {data && data.posts.hasMore && (
         <Flex justifyContent="center">
           <Button
             onClick={() => {
               setVariables({
                 limit: variables.limit,
-                cursor: postData.posts.posts[postData.posts.posts.length - 1].createdAt,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
               });
             }}
             isLoading={fetching}
